84b53d943b771ec830b7d43142e16cddfba00924
Praween <not001praween001@gmail.com>
2014-07-30 19:20:37 +0900

    Update sqlite extender. Add login capability.

--- bin/Httpd.sh
+++ bin/Httpd.sh
@@ -3,12 +3,12 @@ export LD_LIBRARY_PATH=SkyBlue/LCP/EDLoggingControl/LogDecompressor:${LD_LIBRARY
 
 #If do not pass a argument.
 if [ ! "$1" ]; then 
-	java -jar Httpd.jar
+    java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd
 else
 	#If argument is not file data path. 
 	if [ ! -f "$1" ]; then 
-		java -jar Httpd.jar
+		java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd
 	else
-		java -jar Httpd.jar -conf $1
+		java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd -conf $1
 	fi
 fi
