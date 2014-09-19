88088a74b7d32107263733ffa7beff87e9e7bb71
Praween <not001praween001@gmail.com>
2014-09-19 10:54:15 +0900

    Update execution shell.

--- bin/HttpdAsDeamon.sh
+++ bin/HttpdAsDeamon.sh
@@ -2,12 +2,12 @@ export LD_LIBRARY_PATH=SkyBlue/LCP/EDLoggingControl/LogDecompressor:${LD_LIBRARY
 
 #If do not pass a argument.
 if [ ! "$1" ]; then 
-    nohup java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd >> /dev/null 2>&1
+    sudo nohup java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd >> /dev/null 2>&1
 else
 	#If argument is not file data path. 
 	if [ ! -f "$1" ]; then 
-	    nohup java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd >> /dev/null 2>&1
+	    sudo nohup java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd >> /dev/null 2>&1
 	else
-	    nohup java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd -conf $1 >> /dev/null 2>&1
+	    sudo nohup java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd -conf $1 >> /dev/null 2>&1
 	fi
 fi
