472843cc30a9024df6aff219008e86a884f9db6a
Praween <not001praween001@gmail.com>
2015-02-22 04:35:54 +0900

    Update edAccessToken feature.

--- bin/Httpd.sh
+++ bin/Httpd.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=$(pwd)/SkyBlue/LCP/EDLoggingControl/LogDecompressor:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=bin/SkyBlue/LCP/EDLoggingControl/LogDecompressor:${LD_LIBRARY_PATH}
 
 #If do not pass a argument.
 if [ ! "$1" ]; then 
-    sudo java -Djava.library.path=$(pwd)/SkyBlue/LCP/EDLoggingControl/LogDecompressor -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd
+	#java -cp .:lib/sqlite-jdbc-3.7.2.jar -jar Httpd.jar
+	java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd
 else
 	#If argument is not file data path. 
 	if [ ! -f "$1" ]; then 
-		sudo java -Djava.library.path=$(pwd)/SkyBlue/LCP/EDLoggingControl/LogDecompressor -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd
+		#java -cp .:lib/sqlite-jdbc-3.7.2.jar -jar Httpd.jar
+		java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd
 	else
-		sudo java -Djava.library.path=$(pwd)/SkyBlue/LCP/EDLoggingControl/LogDecompressor -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd -conf $1
+		#java -cp .:lib/sqlite-jdbc-3.7.2.jar -jar Httpd.jar -conf $1
+		java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd -conf $1
 	fi
 fi
