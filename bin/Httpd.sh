0d0ee4e6366403b69e208f0bdb704d132b967e86
Praween <not001praween001@gmail.com>
2015-02-28 17:45:32 +0900

    Update automatic preparing the resource of server pushing feature.

--- bin/Httpd.sh
+++ bin/Httpd.sh
@@ -4,14 +4,14 @@ export LD_LIBRARY_PATH=bin/SkyBlue/LCP/EDLoggingControl/LogDecompressor:${LD_LIB
 #If do not pass a argument.
 if [ ! "$1" ]; then 
 	#java -cp .:lib/sqlite-jdbc-3.7.2.jar -jar Httpd.jar
-	java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd
+	java -cp .:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar SkyBlue.LCP.Httpd.core.Httpd
 else
 	#If argument is not file data path. 
 	if [ ! -f "$1" ]; then 
 		#java -cp .:lib/sqlite-jdbc-3.7.2.jar -jar Httpd.jar
-		java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd
+		java -cp .:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar SkyBlue.LCP.Httpd.core.Httpd
 	else
 		#java -cp .:lib/sqlite-jdbc-3.7.2.jar -jar Httpd.jar -conf $1
-		java -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd -conf $1
+		java -cp .:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar SkyBlue.LCP.Httpd.core.Httpd -conf $1
 	fi
 fi
--- bin/HttpdAsDeamon.sh
+++ bin/HttpdAsDeamon.sh
@@ -1,13 +1,16 @@
-export LD_LIBRARY_PATH=$(pwd)/SkyBlue/LCP/EDLoggingControl/LogDecompressor:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=bin/SkyBlue/LCP/EDLoggingControl/LogDecompressor:${LD_LIBRARY_PATH}
 
 #If do not pass a argument.
 if [ ! "$1" ]; then 
-    sudo sh -c "java -Djava.library.path=$(pwd)/SkyBlue/LCP/EDLoggingControl/LogDecompressor -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd -deamon >> /dev/null 2>&1 &"
+	#nohup java -jar Httpd.jar >> /dev/null 2>&1
+	sudo sh -c "java -cp .:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar SkyBlue.LCP.Httpd.core.Httpd -deamon >> /dev/null 2>&1 &"
 else
 	#If argument is not file data path. 
 	if [ ! -f "$1" ]; then 
-	    sudo sh -c "java -Djava.library.path=$(pwd)/SkyBlue/LCP/EDLoggingControl/LogDecompressor -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd -deamon >> /dev/null 2>&1 &"
+		#nohup java -jar Httpd.jar >> /dev/null 2>&1
+		sudo sh -c "java -cp .:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar SkyBlue.LCP.Httpd.core.Httpd -deamon >> /dev/null 2>&1 &"
 	else
-	    sudo su -c "java -Djava.library.path=$(pwd)/SkyBlue/LCP/EDLoggingControl/LogDecompressor -cp .:lib/sqlite-jdbc-3.7.2.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar SkyBlue.LCP.Httpd.core.Httpd -conf $1 -deamon >> /dev/null 2>&1 &"
+		#nohup java -jar Httpd.jar -conf $1 >> /dev/null 2>&1
+		sudo sh -c "java -cp .:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar -classpath Httpd.jar:lib/sqlite-jdbc-3.7.2.jar:lib/java_websocket.jar SkyBlue.LCP.Httpd.core.Httpd -conf $1 -deamon >> /dev/null 2>&1 &"
 	fi
 fi
