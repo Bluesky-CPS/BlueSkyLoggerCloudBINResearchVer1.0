a6a565245ccac2eabeb0bdd7607a90064d91dab3
Praween <not001praween001@gmail.com>
2015-06-17 19:16:31 +0900

    Update style of codemirror.

--- bin/Inetpub/codemirror/codemirror-5.3/mode/javascript/javascript.js
+++ bin/Inetpub/codemirror/codemirror-5.3/mode/javascript/javascript.js
@@ -362,15 +362,24 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
   function expressionInner(type, noComma) {
     if (cx.state.fatArrowAt == cx.stream.start) {
       var body = noComma ? arrowBodyNoComma : arrowBody;
-      if (type == "(") return cont(pushcontext, pushlex(")"), commasep(pattern, ")"), poplex, expect("=>"), body, popcontext);
+      if (type == "(") {
+		  console.log("expressionInner(type, noComma) type is \"(\"");
+		  return cont(pushcontext, pushlex(")"), commasep(pattern, ")"), poplex, expect("=>"), body, popcontext);
+	  }
       else if (type == "variable") return pass(pushcontext, pattern, expect("=>"), body, popcontext);
     }
 
     var maybeop = noComma ? maybeoperatorNoComma : maybeoperatorComma;
     if (atomicTypes.hasOwnProperty(type)) return cont(maybeop);
-    if (type == "function") return cont(functiondef, maybeop);
+    if (type == "function") {
+		console.log("expressionInner \"function\"");
+		return cont(functiondef, maybeop);
+	}
     if (type == "keyword c") return cont(noComma ? maybeexpressionNoComma : maybeexpression);
-    if (type == "(") return cont(pushlex(")"), maybeexpression, comprehension, expect(")"), poplex, maybeop);
+    if (type == "(") {
+		console.log("expressionInner \"(\"");
+		return cont(pushlex(")"), maybeexpression, comprehension, expect(")"), poplex, maybeop);
+	}
     if (type == "operator" || type == "spread") return cont(noComma ? expressionNoComma : expression);
     if (type == "[") return cont(pushlex("]"), arrayLiteral, poplex, maybeop);
     if (type == "{") return contCommasep(objprop, "}", null, maybeop);
@@ -402,7 +411,10 @@ CodeMirror.defineMode("javascript", function(config, parserConfig) {
     if (type == "quasi") { return pass(quasi, me); }
     if (type == ";") return;
     if (type == "(") return contCommasep(expressionNoComma, ")", "call", me);
-    if (type == ".") return cont(property, me);
+    if (type == ".") {
+		console.log(" maybeoperatorNoComma(type, value, noComma)");
+		return cont(property, me);
+	}
     if (type == "[") return cont(pushlex("]"), maybeexpression, expect("]"), poplex, me);
   }
   function quasi(type, value) {
